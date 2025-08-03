3b5a505357f5b2bbc89c4f02da77ed6530be066f
Khalil Hafz <khalilhafz@gmail.com>
2025-08-03 15:52:41 +0300

    setup

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 
                 const removeButton = document.createElement('button');
                 removeButton.textContent = 'Remove';
-                removeButton.className =  'remove-btn';
+                removeButton.classList.add('remove-btn')  ;
                 removeButton.onclick = () => {
                     newTask.remove();
                 }
